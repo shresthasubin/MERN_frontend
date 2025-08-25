8f086bffb1c684e5b0969ad441e3cb5a5901a03a
shresthasubin <shresthasubin938@gmail.com>
2025-08-25 10:32:55 +0545

    changes in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/MERN_frontend"
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH
 })
